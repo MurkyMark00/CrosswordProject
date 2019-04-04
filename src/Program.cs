8ded7b5189c0c31ddd8c81088560f3d442174836
MurkyMark <murkymarkymark@gmail.com>
2019-04-04 19:33:04 +0300

    Initialized Project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrosswordProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
